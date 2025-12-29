112e480a64a5e61eca6f95eed2020742cb684e2e
Gokce Can <zgokcecann@hotmail.com>
2025-12-29 13:26:58 +0300

    added vue-router and home page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 import store from './store'
 import './style.css'
 
-createApp(App).use(store).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
